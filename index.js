2d582e4ceb44abb13d7af693752b825b8a17d3d2
karissa <krmckelv@gmail.com>
2017-02-22 18:18:27 -0800

    make sure an object copy is made for each rest call

--- index.js
+++ index.js
@@ -31,30 +31,34 @@ function API (opts) {
   }
 
   function rest (path) {
-    var opts = {
+    var defaults = {
       uri: api + path,
       json: true
     }
     return {
       get: function (input, cb) {
         var params = qs.stringify(input)
-        opts.uri += '?' + params
+        var opts = Object.assign({}, defaults)
+        opts.uri = defaults.uri + '?' + params
         opts.method = 'GET'
         township.secureRequest(opts, cb)
       },
       create: function (input, cb) {
+        var opts = Object.assign({}, defaults)
         opts.body = input
         opts.method = 'POST'
         township.secureRequest(opts, cb)
       },
       update: function (input, cb) {
         if (!input.id) throw new Error('id required')
+        var opts = Object.assign({}, defaults)
         opts.body = input
         opts.method = 'PUT'
         township.secureRequest(opts, cb)
       },
       delete: function (input, cb) {
         if (!input.id) throw new Error('id required')
+        var opts = Object.assign({}, defaults)
         opts.body = input
         opts.method = 'DELETE'
         township.secureRequest(opts, cb)
